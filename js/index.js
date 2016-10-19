9602b2379977ce48713b524e19b217954d6626f6
alexh-name <1@alexh.name>
2016-10-19 18:30:25 +0200

    typos

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ var mm_counter = 0;
 function compose() {
   var date = new Date();
   new_msg = 
-    "\f"+"Reply-To: <crew-post@jugendrettet.org>"+"\n"
+    "\f"+"Reply-To: <crewpost@jugendrettet.org>"+"\n"
     +"Date: "+date+"\n"
     +"From: "+$("#from").val()+"\n"
     +"To: "+$("#to").val()+"\n"
